52f67323f22187f56f80f5c4e9ac0c365befbec6
Paulo Alex <garbagames@gmail.com>
2024-11-09 20:58:39 -0300

    change data

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ def download_highest_resolution(url):
     ydl_opts = {
         'format': 'bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]/best[ext=mp4][height<=1080]',
         # Preferência para MP4 e qualidade até 1080p
-        'outtmpl': 'C:\\Users\\garba\\Videos\\%(title)s.%(ext)s',  # Local e nome do arquivo de saída
+        'outtmpl': 'Local_de_preferência\\%(title)s.%(ext)s',  # Local e nome do arquivo de saída
         'merge_output_format': 'mp4'  # Formato final do arquivo se precisar juntar vídeo e áudio
     }
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 if __name__ == '__main__':
-    VIDEO_URL = 'https://www.youtube.com/watch?v=zEcehthgR5s&ab_channel=FelipeRocha%E2%80%A2FullStackClub'
+    VIDEO_URL = 'link_do_video'
     download_highest_resolution(VIDEO_URL)
